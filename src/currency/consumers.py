29d103d9eb11346f741024fb6d0c6c5e85b398f7
Kozin Artem <grot_artem@inbox.ru>
2024-07-07 18:15:00 +0300

    reversed self.messages and add initial api_request to consumer

--- src/currency/consumers.py
+++ src/currency/consumers.py
@@ -1,24 +1,34 @@
 import json
+from collections import OrderedDict
+from datetime import datetime
 
 from channels.generic.websocket import AsyncWebsocketConsumer
+
+from .utils import get_currency_api_request
 
 
 class CurrencyConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.messages = {}
-        self.counter = 0
+        self.messages = OrderedDict()
+        self.message_key = datetime.now()
 
     async def connect(self):
         await self.channel_layer.group_add("default", self.channel_name)
         await self.accept()
+        data = get_currency_api_request()
+        await self.channel_layer.group_send(
+            "default",
+            {"type": "chat.message", "message": data},
+        )
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard("default", self.channel_name)
 
     async def chat_message(self, event):
         message = event["message"]
-        self.messages.update({f"{self.counter}": message})
-        self.counter += 1
+        self.message_key = datetime.now()
+        self.messages.update({f"{self.message_key}": message})
+        self.messages.move_to_end(f"{self.message_key}", last=False)
 
         await self.send(text_data=json.dumps({"message": self.messages}))
