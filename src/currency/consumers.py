7d30d810d339c7bac124bba352a3028a0b1feff4
Kozin Artem <grot_artem@inbox.ru>
2024-07-08 18:37:04 +0300

    Add minimal docstrings and type annotationa to CurrencyConsumer

--- src/currency/consumers.py
+++ src/currency/consumers.py
@@ -1,39 +1,56 @@
 from collections import OrderedDict
 from datetime import datetime
 from json import dumps, loads
+from typing import Any, Mapping, OrderedDict
 
+import redis
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.conf import settings
-from redis import from_url
 
 from .utils import get_currency_api_request
 
 
 class CurrencyConsumer(AsyncWebsocketConsumer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.messages = OrderedDict()
-        self.message_key = datetime.now()
-        self.history_vault = from_url(settings.REDIS_URI)
+    """Класс-обработчик событий websocket-соединения."""
 
-    async def connect(self):
+    def __init__(self, *args, **kwargs) -> None:
+        """Метод инициализации класса."""
+
+        super().__init__(*args, **kwargs)
+
+        self.messages: OrderedDict = OrderedDict()
+        self.message_key: datetime = datetime.now()
+        self.history_vault: redis.Redis = redis.from_url(settings.REDIS_URI)
+
+    async def connect(self) -> None:
+        """Метод подключения к websocket-соединению."""
+
         await self.channel_layer.group_add("default", self.channel_name)
         await self.accept()
-        if self.history_vault.exists("default_history"):
-            self.messages.update(loads(self.history_vault.get("default_history")))
+
+        if self.history_vault.exists("default_history") and (
+            (history := self.history_vault.get("default_history")) is not None
+        ):
+            self.messages.update(loads(history))
+
         data = get_currency_api_request()
         await self.channel_layer.group_send(
             "default",
             {"type": "chat.message", "message": data},
         )
 
-    async def disconnect(self, close_code):
+    async def disconnect(self, close_code: Any) -> None:
+        """Метод отключения от websocket-соединения."""
+
         if len(self.messages) == 10:
             self.messages.popitem()
         self.history_vault.setex("default_history", 600, dumps(self.messages))
+
         await self.channel_layer.group_discard("default", self.channel_name)
 
-    async def chat_message(self, event):
+    async def chat_message(self, event: Mapping[str, Any]) -> None:
+        """Метод обработки событий websocket-соединения с типом "chat.message"."""
+
         message = event["message"]
 
         if len(self.messages) == 10:
