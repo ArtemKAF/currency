4fa68310c1eafc83f555ff7b3cb4fe90f2fbac7d
Kozin Artem <grot_artem@inbox.ru>
2024-07-08 19:38:32 +0300

    Add minimal docstring and type annotations to get_currency_task

--- src/currency/tasks.py
+++ src/currency/tasks.py
@@ -1,3 +1,5 @@
+from typing import Any, Optional
+
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 
@@ -7,10 +9,16 @@ from .utils import get_currency_api_request
 
 
 @app.task
-def get_currency_task():
-    message = get_currency_api_request()
-    channel_layer = get_channel_layer("default")
-    async_to_sync(channel_layer.group_send)(
-        "default",
-        {"type": "chat.message", "message": message},
-    )
+def get_currency_task() -> None:
+    """
+    Метод получения у стороннего API данных о курсе пары доллар-рубль и
+    отправки их в канал определенный в настройках как "default".
+    """
+
+    message: dict = get_currency_api_request()
+    channel_layer: Optional[Any] = get_channel_layer("default")
+    if channel_layer is not None:
+        async_to_sync(channel_layer.group_send)(
+            "default",
+            {"type": "chat.message", "message": message},
+        )
